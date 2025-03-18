8b2ff395c7358d643be3e0c7bf5a67048f7a0c07
Cian747 <studiokaiser1@gmail.com>
2025-03-18 15:35:13 +0300

    added models to admin folder for superuser

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Sport,Location,Profile,Comment,Wishlist,SportAdvert
+from django.contrib.auth.models import User
 # Register your models here.
+
+admin.site.register(Profile)
+admin.site.register(Location)
+admin.site.register(Comment)
+admin.site.register(Sport)
+admin.site.register(SportAdvert)
+admin.site.register(Wishlist)
