83015997916814d038357be6de9cf9c860013178
Cian747 <studiokaiser1@gmail.com>
2025-03-18 15:34:00 +0300

    Restructed the tables for seamless user experience. Renaming and added extra fields

--- app/models.py
+++ app/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 from django.urls import reverse
-from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
@@ -38,17 +37,28 @@ class Location(models.Model):
         return self.name
 
 class Profile(models.Model):
+    ADMIN = 1
+    STUDENT = 2
+
+    USER_ROLE_CHOICES = (
+        (ADMIN, 'Coach'),
+        (STUDENT, 'Client'),
+    )
+
     user = models.OneToOneField(User, on_delete=models.CASCADE, unique=False)
     sport = models.ForeignKey(Sport, on_delete=models.SET_NULL, null=True, blank=True)
     location = models.ForeignKey(Location, on_delete=models.SET_NULL, null=True, blank=True)
+    role = models.PositiveSmallIntegerField(choices=USER_ROLE_CHOICES, blank=True, null=True, default=2)
     profile_photo = CloudinaryField('profile_image',null=True, blank=True)
     bio = models.TextField(blank=True, null=True)
     phone_number = models.CharField(max_length=15, blank=True, null=True)
     specialization = models.CharField(max_length=255, blank=True, null=True)
     certifications = models.TextField(blank=True, null=True)
     experience_years = models.PositiveIntegerField(default=0)
+    price_per_hour = models.DecimalField(max_digits=6, decimal_places=2,blank=True,null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    likes = models.ManyToManyField(User, related_name='supporters')
 
     def __str__(self) -> str:
         return f"{self.user.username} - {self.sport.name if self.sport else 'No Sport'}"
@@ -78,26 +88,26 @@ class Profile(models.Model):
         return cls.objects.filter(user__username__icontains = search_term)
 
 
-    # def get_absolute_url(self):
-    #     return reverse('profile',args=[str(self.id)])
+    def get_absolute_url(self):
+        return reverse('profile',args=[str(self.id)])
 
         # return reverse('home')
     
     def __str__(self) -> str:
        return self.user.username
 
-class Enquiry(models.Model):
-    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
-    question = models.TextField()
-    sent_on = models.DateTimeField(auto_now_add=True)
+# class Enquiry(models.Model):
+#     profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+#     question = models.TextField()
+#     sent_on = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return self.profile.user.username
+#     def __str__(self):
+#         return self.profile.user.username
 
-    def save_enquiry(self):
-        return self.save()
+#     def save_enquiry(self):
+#         return self.save()
 
-class Service(models.Model):
+class SportAdvert(models.Model):
     sport_punchline = models.CharField(max_length=50)
     service_image = CloudinaryField('image',blank=True,null=True)
     related_sport = models.ForeignKey(Sport, on_delete=models.CASCADE)
@@ -115,10 +125,11 @@ class Service(models.Model):
 
 class Wishlist(models.Model):
     '''
-    A user can add services they are interested in following
+    A user can add services they are interested in following or trying out
     '''
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
-    service_name = models.ForeignKey(Service,on_delete=models.CASCADE)
+    service_name = models.ForeignKey(SportAdvert,on_delete=models.CASCADE)
+    sport = models.ForeignKey(Sport,on_delete=models.CASCADE)
     saved_on = models.DateTimeField(auto_now_add=True)
 
     def __str__(self) -> str:
@@ -129,7 +140,8 @@ class Wishlist(models.Model):
 
 class Comment(models.Model):
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
-    comment = models.TextField()
+    text = models.TextField()
+    rating = models.IntegerField(default=0,blank=True,null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self) -> str:
